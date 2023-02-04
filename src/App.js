0a9d160996a08d8ad45beb48afb3266fddfe06da
Phu <vusiphu20042000@gmail.com>
2023-02-04 23:10:09 +0700

    1 commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let name = 'phu';
+  let number = 2022;
+  let obj = { name: 'react', id: '1' }
+  let link = 'https://www.youtube.com/watch?v=Y9gTouaZJ5s&list=PLncHg6Kn2JT4xzJyhXfmJ53dzwVbq-S_E&index=9&ab_channel=H%E1%BB%8FiD%C3%A2nIT';
   return (
     <div className="App">
+      {console.log(obj)}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p>Hello id: {obj.id} name {name} in {number}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <a href={link}> link ne</a>
+
       </header>
     </div>
   );
